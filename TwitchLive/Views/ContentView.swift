3f47e8afcbc4c517e2936128aca33a0279a73217
Mikey <shpoopdy@gmail.com>
2025-02-27 11:36:30 -0800

    Add login page styling

--- TwitchLive/Views/ContentView.swift
+++ TwitchLive/Views/ContentView.swift
@@ -8,20 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var username = ""
+  @State private var password = ""
+  
   var body: some View {
     VStack {
+      Text("Welcome!")
+        .font(.largeTitle)
+        .fontWeight(.black)
+        .padding(.bottom, 42)
+      HStack {
+        Text("Username")
+        TextField(
+          "Username",
+          text: $username
+        )
+      }
+      .padding()
+      
+      HStack {
+        Text("Password")
+        TextField(
+          "Username",
+          text: $password
+        )
+      }
+      .padding()
+      
       Button (action: {
         print("You pressed me!")
       }, label: {
         Text("Sign In")
+          .fontWeight(.heavy)
+          .font(.title3)
+          .frame(maxWidth: .infinity)
+          .foregroundStyle(.white)
+          .background(LinearGradient(gradient: Gradient(colors: [.pink, .blue]), startPoint: .leading, endPoint: .trailing))
+          .cornerRadius(40)
+        
       })
       .padding()
       
-      Button (action: {
-        print("You pressed me!!!")
-      }, label: {
-        Text("Sign Up")
-      })
+      HStack {
+        Spacer()
+        Text("Forgotten Password?")
+          .fontWeight(.thin)
+          .foregroundColor(Color.blue)
+          .underline()
+      }
+      .padding(.top, 16)
+      
     }
   }
 }
